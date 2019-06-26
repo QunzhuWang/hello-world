f578cd30123c012935f0000e9efde748115aaa1d
QunzhuWang <ff664693506@gmail.com>
2019-06-26 13:33:25 +0800

    come

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const NotFoundPage =()=> (
+    <p>You just hit a route that desn&#39;t exist..</p>
+
+)
+
+export default NotFoundPage
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Contact = () => (
+    <div>
+        <h1>This is contact page</h1>
+        <Link to="/">Back to homepage</Link>
+    </div>
+)
+
+export default Contact
